ef3022480e8cae7509290474dfa002e592cc91ef
root <root@pop-os.localdomain>
2024-03-16 22:21:06 -0700

    init commnit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_CG65.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_CG65-user \
+    omni_CG65-userdebug \
+    omni_CG65-eng
