8eede09cb52a1955e2cdf6031e8c74cf6021dba3
1ndev-ui <1ndevelopment@protonmail.com>
2024-03-20 15:49:53 -0700

    Removed TWRP build processes off Android.mk

--- Android.mk
+++ Android.mk
@@ -11,27 +11,3 @@ ifeq ($(TARGET_DEVICE),CG65)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
 
-ifneq ($(TW_OVERRIDE_SYSTEM_PROPS),)
-    TW_INCLUDE_LIBRESETPROP := true
-    LOCAL_CFLAGS += -DTW_OVERRIDE_SYSTEM_PROPS=$(TW_OVERRIDE_SYSTEM_PROPS)
-endif
-
-ifneq ($(TW_INCLUDE_LIBRESETPROP),)
-    LOCAL_SHARED_LIBRARIES += libresetprop
-    LOCAL_C_INCLUDES += external/magisk-prebuilt/include
-    LOCAL_CFLAGS += -DTW_INCLUDE_LIBRESETPROP
-endif
-
-ifeq ($(TW_INCLUDE_FASTBOOTD), true)
-    LOCAL_CFLAGS += -DTW_INCLUDE_FASTBOOTD
-endif
-
-ifeq ($(TW_INCLUDE_RESETPROP), true)
-TWRP_REQUIRED_MODULES += \
-    resetprop
-endif
-
-ifeq ($(TW_INCLUDE_LIBRESETPROP), true)
-TWRP_REQUIRED_MODULES += \
-    libresetprop
-endif
